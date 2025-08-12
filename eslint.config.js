9f3778db8bc3065265cc38ebc31b633822fed05a
makarand <makarand@gmail.com>
2025-08-12 14:46:20 -0700

    wip

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+import importPlugin from 'eslint-plugin-import';
 
 export default tseslint.config(
   { ignores: ['dist', 'coverage', 'node_modules', 'public'] },
@@ -16,6 +17,12 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      import: importPlugin,
+    },
+    settings: {
+      'import/resolver': {
+        typescript: true,
+      },
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
